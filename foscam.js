df61cc734ce24c1ad7c9a24f394b421d089d0944
Franklin van de Meent <fr@nkl.in>
2014-12-17 09:26:48 +0100

    Cleaner setEncoding

--- foscam.js
+++ foscam.js
@@ -390,7 +390,7 @@ app.talk = function( props ) {
   }, function( response ) {
 
     // response
-    response.setEncoding( props.encoding ? props.encoding : 'utf8' )
+    response.setEncoding( props.encoding || 'utf8' )
     var data = ''
 
     response.on( 'data', function( chunk ) { data += chunk })
